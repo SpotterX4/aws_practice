a500f3ca03488421c0271ea3d06cbf7ed988143d
pehardy <pehardy@ncsu.edu>
2018-11-28 08:55:22 -0500

    initializing project

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+var server = app.listen(9001, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("listening at http://%s%s", host, port);
+});
